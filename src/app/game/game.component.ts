612f57a2e82acb3276b1427b81c7de3110455303
Alex <saez0724+github@googlemail.com>
2024-07-23 13:02:20 +0200

    added players and animations

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
+  pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game = new Game();
+
+  cardlist = [
+    {
+      name: "Cover",
+      img: "card_cover.png"      
+    }
+  ];
+
+  playerList = [];
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      if (this.currentCard) { // Ensure currentCard is not undefined
+        this.pickCardAnimation = true;
+        console.log('New Card: ' + this.currentCard);
+        console.log('This Game: ', this.game);
+
+        setTimeout(() => {
+          if (this.currentCard) { // Ensure currentCard is not undefined
+            this.game.playedCard.push(this.currentCard);
+          }
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
 }
